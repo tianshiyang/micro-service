c0ef46dc2c1af51d863e4f20559e26dd5afe3db8
田世洋 <tianshiyang@icourt.cc>
2023-11-18 21:59:56 +0800

    feat: 三个应用初始化完成

--- micro-app-vue3/src/views/HomeView.vue
+++ micro-app-vue3/src/views/HomeView.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <HelloWorld msg="vue3子应用"/>
   </div>
 </template>
 
--- micro-app-vue3/vue.config.js
+++ micro-app-vue3/vue.config.js
@@ -2,6 +2,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    port: 6000
+    port: 7000
   }
 })
