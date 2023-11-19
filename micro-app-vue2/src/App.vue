398d22482fde5220ad88aab9d1653ba69e92f6e9
田世洋 <tianshiyang@icourt.cc>
2023-11-19 20:41:05 +0800

    feat: 删除测试代码

--- micro-app-vue2/src/App.vue
+++ micro-app-vue2/src/App.vue
@@ -1,20 +1,8 @@
 <template>
   <div id="app">
-    <HelloWorld></HelloWorld>
     <router-view></router-view>
   </div>
 </template>
-
-<script>
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
-</script>
 
 <style scoped>
 </style>
